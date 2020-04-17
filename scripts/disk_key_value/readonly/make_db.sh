7579c576011cf6ff5c370fe5f6cd36a87d72bac7
Matt Hagy <matthew.hagy@gmail.com>
2020-04-17 06:49:59 -0400

    additional results and analysis

--- scripts/disk_key_value/readonly/make_db.sh
+++ scripts/disk_key_value/readonly/make_db.sh
@@ -7,22 +7,21 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
-#sudo file -s /dev/nvme0n1
-#sudo mkfs -t xfs /dev/nvme0n1 
-#sudo mkdir /data
-#sudo mount /dev/nvme0n1 /data
-#sudo chown ubuntu:ubuntu /data
-#sudo apt update
-#sudo apt install openjdk-11-jre-headless -y
-#sudo apt-get install python3.8 openjdk-11-jre-headless python3-pip -y
-#python3.8 -m pip install sendgrid
+sudo file -s /dev/nvme0n1
+sudo mkfs -t xfs /dev/nvme0n1 
+sudo mkdir /data
+sudo mount /dev/nvme0n1 /data
+sudo chown ubuntu:ubuntu /data
+sudo apt update
+sudo apt-get install python3.8 openjdk-11-jre-headless python3-pip -y
+python3.8 -m pip install sendgrid
 
 
-VALUE_SIZE=4096
-SORTED_FILE_ENTRY_COUNT=2.429543e+05
+#VALUE_SIZE=4096
+#SORTED_FILE_ENTRY_COUNT=2.429543e+05
 
-# VALUE_SIZE=256
-# SORTED_FILE_ENTRY_COUNT=3.623188e+06
+VALUE_SIZE=256
+SORTED_FILE_ENTRY_COUNT=3.623188e+06
 
 
 SORTED_FILE_COUNT=250
@@ -35,8 +34,8 @@ run() {
     java -classpath dbf0java-1.0-SNAPSHOT-jar-with-dependencies.jar $* || exit 1
 }
 
-#run dbf0.disk_key_value.readonly.WriteSortedKeyValueFiles \
-#    $SORTED_FILE_DIR $SORTED_FILE_COUNT $VALUE_SIZE $SORTED_FILE_ENTRY_COUNT
+run dbf0.disk_key_value.readonly.WriteSortedKeyValueFiles \
+    $SORTED_FILE_DIR $SORTED_FILE_COUNT $VALUE_SIZE $SORTED_FILE_ENTRY_COUNT
 
 for INCLUDE_FILE_COUNT in 10 50 250; do
     DB_DIR=${BASE_DIR}/db${INCLUDE_FILE_COUNT}
